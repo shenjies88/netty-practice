214570ba8e25db52278fd71db5728ae038029a6e
沈杰 <shenjie@yunhx.net>
2020-03-03 09:12:53 +0800

    fix

--- src/main/java/com/shenjies88/netty/handler/TimeClientHandler.java
+++ src/main/java/com/shenjies88/netty/handler/TimeClientHandler.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         System.out.println("接收消息");
